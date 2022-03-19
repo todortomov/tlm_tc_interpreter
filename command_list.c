7bced28ed6456c0a40b7cf330bbedcde482f1e15
Todor Tomov <todor.too@gmail.com>
2022-03-19 22:41:42 +0200

    Implement command processing
    Implement the processing of the commands. A queue is used to store the
    telecommands. On insert the command is added to the tail of the queue.
    On execute the commands are removed from the head of the queue and
    executed. A merge sort algorithm is used to sort the queue when a sort
    command is received.
    
    Signed-off-by: Todor Tomov <todor.too@gmail.com>

--- command_list.c
+++ command_list.c
@@ -55,52 +55,343 @@ end_err:
 
 int cl_process_insert_lo(int data, struct cl *cl)
 {
-	return 0;
+	int prio = CMD_PRIO_MIN;
+
+	return cl_process_insert(prio, data, cl);
 }
 
 int cl_process_insert(int prio, int data, struct cl *cl)
 {
+	struct node *new;
+
+	if (cl == NULL) {
+		printf("%s: cl == NULL\n", __func__);
+		return 1;
+	}
+
+	new = (struct node *) malloc(sizeof(struct node));
+	if (new == NULL) {
+		printf("%s: malloc failed.\n", __func__);
+		return 1;
+	}
+
+	if (idg_get_id_next(cl->idg, &new->entry_id) != 0) {
+		printf("%s: entry id generation failed.\n", __func__);
+		free(new);
+		return 1;
+	}
+	new->priority = prio;
+	new->data = data;
+	new->next = NULL;
+
+	if (cl->head == NULL) {
+		cl->head = new;
+		cl->tail = new;
+	} else {
+		cl->tail->next = new;
+		cl->tail = new;
+	}
+
 	return 0;
 }
 
 int cl_process_delete(int entry_id, struct cl *cl)
 {
+	struct node *prev, *curr;
+
+	if (cl == NULL) {
+		printf("%s: cl == NULL\n", __func__);
+		return 1;
+	}
+
+	if (cl->head == NULL) {
+		printf("%s: node with entry_id = %d not found, list empty\n",
+		       __func__, entry_id);
+		return 1;
+	}
+
+	if (cl->head == cl->tail) {
+		if (cl->head->entry_id == entry_id) {
+			free(cl->head);
+			cl->head = NULL;
+			cl->tail = NULL;
+			return 0;
+		} else {
+			printf("%s: node with entry_id = %d not found.\n",
+			       __func__, entry_id);
+			return 1;
+		}
+	}
+
+	curr = cl->head;
+
+	if (curr->entry_id == entry_id) {
+		cl->head = curr->next;
+		free(curr);
+		return 0;
+	}
+
+	prev = cl->head;
+	curr = cl->head->next;
+
+	while (curr != NULL && curr->entry_id != entry_id) {
+		prev = curr;
+		curr = curr->next;
+	}
+
+	if (curr == NULL) {
+		printf("%s: node with entry_id = %d not found.\n",
+		       __func__, entry_id);
+		return 1;
+	}
+
+	if (curr->next == NULL) {
+		prev->next = NULL;
+		cl->tail = prev;
+	} else {
+		prev->next = curr->next;
+	}
+
+	free(curr);
+
 	return 0;
+}
+
+static void list_split(struct node *head, struct node **first, struct node **second)
+{
+	struct node *fast, *slow;
+
+	if (head == NULL) {
+		*first = *second = NULL;
+		return;
+	}
+
+	if (head->next == NULL) {
+		*first = head;
+		*second = NULL;
+		return;
+	}
+
+	slow = head;
+	fast = head->next;
+	while (fast != NULL) {
+		fast = fast->next;
+		if (fast != NULL) {
+			fast = fast->next;
+			slow = slow->next;
+		}
+	}
+
+	*first = head;
+	*second = slow->next;
+	slow->next = NULL;
+}
+
+static void list_merge(struct node *first, struct node *second, struct node **merged,
+		       int (*compar)(struct node *a, struct node *b))
+{
+	struct node *tail;
+
+	if (first == NULL) {
+		*merged = second;
+		return;
+	}
+
+	if (second == NULL) {
+		*merged = first;
+		return;
+	}
+
+	if (compar(first, second) <= 0) {
+		*merged = first;
+		first = first->next;
+	} else {
+		*merged = second;
+		second = second->next;
+	}
+
+	tail = *merged;
+
+	while (1) {
+		if (first == NULL) {
+			tail->next = second;
+			break;
+		} else if (second == NULL) {
+			tail->next = first;
+			break;
+		} else if (compar(first, second) <= 0) {
+			tail->next = first;
+			tail = tail->next;
+			first = first->next;
+		} else {
+			tail->next = second;
+			tail = tail->next;
+			second = second->next;
+		}
+	}
+}
+
+static int compare_node(struct node *a, struct node *b)
+{
+	if (a->priority > b->priority) {
+		return -1;
+	} else if (a->priority < b->priority) {
+		return 1;
+	} else if (a->data < b->data) {
+		return -1;
+	} else if (a->data > b->data) {
+		return 1;
+	} else {
+		return 0;
+	}
+}
+
+static void list_sort(struct node **head_ref)
+{
+	struct node *head = *head_ref;
+	struct node *first, *second;
+
+	if (head->next == NULL)
+		return;
+
+	list_split(head, &first, &second);
+
+	list_sort(&first);
+	list_sort(&second);
+
+	list_merge(first, second, head_ref, compare_node);
 }
 
 int cl_process_sort(struct cl *cl)
 {
+	if (cl == NULL) {
+		printf("%s: cl == NULL\n", __func__);
+		return 1;
+	}
+
+	if (cl->head == NULL)
+		return 0;
+
+	list_sort(&cl->head);
+
+	cl->tail = cl->head;
+	while (cl->tail->next != NULL)
+		cl->tail = cl->tail->next;
+
 	return 0;
 }
 
 int cl_process_modify(int entry_id, int data, struct cl *cl)
 {
+	struct node *a;
+
+	if (cl == NULL) {
+		printf("%s: cl == NULL\n", __func__);
+		return 1;
+	}
+
+	a = cl->head;
+	while (a != NULL && a->entry_id != entry_id)
+		a = a->next;
+
+	if (a == NULL) {
+		printf("%s: node with entry_id = %d not found.\n",
+		       __func__, entry_id);
+		return 1;
+	}
+
+	a->data = data;
+
 	return 0;
 }
 
 int cl_process_print(struct cl *cl)
 {
+	struct node *a;
+
+	if (cl == NULL) {
+		printf("%s: cl == NULL\n", __func__);
+		return 1;
+	}
+
+	a = cl->head;
+	while (a != NULL) {
+		printf("(%d, %d, %d)\n", a->entry_id, a->priority, a->data);
+		a = a->next;
+	}
+
 	return 0;
 }
 
 int cl_process_reverse(struct cl *cl)
 {
+	struct node *first, *second, *third;
+
+	if (cl == NULL) {
+		printf("%s: cl == NULL\n", __func__);
+		return 1;
+	}
+
+	if (cl->head == NULL || cl->head->next == NULL)
+		return 0;
+
+	first = cl->head;
+	second = first->next;
+	third = second->next;
+
+	cl->tail = first;
+	first->next = NULL;
+	second->next = first;
+
+	while (third != NULL) {
+		first = second;
+		second = third;
+		third = third->next;
+
+		second->next = first;
+	}
+
+	cl->head = second;
+
 	return 0;
 }
 
 int cl_process_execute(struct cl *cl)
 {
+	struct node *a;
+
+	if (cl == NULL) {
+		printf("%s: cl == NULL\n", __func__);
+		return 1;
+	}
+
+	while (cl->head != NULL) {
+		/* execute cl->head here */
+		a = cl->head;
+		cl->head = cl->head->next;
+		free(a);
+	}
+	cl->tail = NULL;
+
 	return 0;
 }
 
 void cl_destroy(struct cl *cl)
 {
+	struct node *a;
+
 	if (cl == NULL) {
 		printf("%s: cl == NULL\n", __func__);
 		return;
 	}
 
 	idg_destroy(cl->idg);
+
+	while (cl->head != NULL) {
+		a = cl->head;
+		cl->head = cl->head->next;
+		free(a);
+	}
 
 	free(cl);
 }
